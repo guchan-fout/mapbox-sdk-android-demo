d179e3376db478401f6fd6cf3982add7f55bd08b
guchan-fout <stevengc@gmail.com>
2022-12-21 17:48:04 +0900

    no message

--- Demo/app/src/main/java/com/example/mapbox/demo/MapActivity.kt
+++ Demo/app/src/main/java/com/example/mapbox/demo/MapActivity.kt
@@ -8,31 +8,37 @@ import android.location.LocationRequest
 import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.tasks.CancellationTokenSource
-import com.mapbox.api.tilequery.MapboxTilequery
-import com.mapbox.common.TileRegionLoadOptions
-import com.mapbox.geojson.FeatureCollection
+import com.mapbox.android.gestures.MoveGestureDetector
 import com.mapbox.geojson.Point
 import com.mapbox.maps.*
+import com.mapbox.maps.extension.observable.eventdata.CameraChangedEventData
 import com.mapbox.maps.extension.observable.eventdata.MapIdleEventData
-import com.mapbox.maps.plugin.attribution.attribution
+import com.mapbox.maps.extension.observable.eventdata.MapLoadedEventData
+import com.mapbox.maps.plugin.LocationPuck2D
+import com.mapbox.maps.plugin.delegates.listeners.OnCameraChangeListener
 import com.mapbox.maps.plugin.delegates.listeners.OnMapIdleListener
-import com.mapbox.maps.plugin.locationcomponent.LocationComponentPlugin
-import com.mapbox.maps.plugin.locationcomponent.LocationComponentPlugin2
+import com.mapbox.maps.plugin.delegates.listeners.OnMapLoadedListener
+import com.mapbox.maps.plugin.gestures.OnMoveListener
+import com.mapbox.maps.plugin.gestures.gestures
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorBearingChangedListener
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener
 import com.mapbox.maps.plugin.locationcomponent.location
 import com.mapbox.maps.plugin.locationcomponent.location2
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.mapbox.maps.extension.style.expressions.dsl.generated.interpolate
+import com.mapbox.maps.plugin.animation.camera
 import timber.log.Timber
 
 
-class MapActivity : AppCompatActivity(),OnMapIdleListener {
+class MapActivity : AppCompatActivity(), OnMapIdleListener, OnMapLoadedListener,
+    OnCameraChangeListener {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     private lateinit var mapView: MapView
@@ -40,41 +46,91 @@ class MapActivity : AppCompatActivity(),OnMapIdleListener {
     private var latitude: Double = 0.0
     private lateinit var retrieveBtn: Button
 
+    private val onIndicatorBearingChangedListener = OnIndicatorBearingChangedListener {
+        mapView.getMapboxMap().setCamera(CameraOptions.Builder().bearing(it).build())
+    }
+
+    private val onIndicatorPositionChangedListener = OnIndicatorPositionChangedListener {
+        mapView.getMapboxMap().setCamera(CameraOptions.Builder().center(it).build())
+        mapView.gestures.focalPoint = mapView.getMapboxMap().pixelForCoordinate(it)
+
+        Timber.d( "now zoom is" + mapView.getMapboxMap().cameraState.zoom)
+
+
+    }
+
+    private val onMoveListener = object : OnMoveListener {
+        override fun onMoveBegin(detector: MoveGestureDetector) {
+            onCameraTrackingDismissed()
+        }
+
+        override fun onMove(detector: MoveGestureDetector): Boolean {
+            return false
+        }
+
+        override fun onMoveEnd(detector: MoveGestureDetector) {}
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
         mapView = MapView(this)
 
-       // mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS)
+
+        // mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS)
         mapView.getMapboxMap().loadStyleUri(
-            Style.MAPBOX_STREETS,
+            Style.DARK,
+            //"mapbox://styles/chan-gu/clbnb2ak1000514o6w0euv8ni",
             // After the style is loaded, initialize the Location component.
+
             object : Style.OnStyleLoaded {
+
                 override fun onStyleLoaded(style: Style) {
                     var params: ViewGroup.LayoutParams = ViewGroup.LayoutParams(
                         ViewGroup.LayoutParams.MATCH_PARENT,
                         ViewGroup.LayoutParams.MATCH_PARENT
                     )
                     addContentView(mapView, params)
+                    //initLocationComponent()
+                    setupGesturesListener()
+
                     mapView.location.updateSettings {
-                        //enabled = true
-                        Timber.d("updateSettings1")
+                        enabled = true
+
+                        this.locationPuck = LocationPuck2D(
+                            bearingImage = AppCompatResources.getDrawable(
+                                this@MapActivity,
+                                R.drawable.mapbox_user_puck_icon,
+                            ),
+                            shadowImage = AppCompatResources.getDrawable(
+                                this@MapActivity,
+                                R.drawable.mapbox_user_icon_shadow,
+                            ),
+                            scaleExpression = interpolate {
+                                linear()
+                                zoom()
+                                stop {
+                                    literal(0.0)
+                                    literal(0.6)
+                                }
+                                stop {
+                                    literal(20.0)
+                                    literal(1.0)
+                                }
+                            }.toJson()
+                        )
                     }
+                    mapView.location.addOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
+                    mapView.location.addOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
                 }
             }
         )
 
         //mapView.attribution.getMapAttributionDelegate().telemetry().setUserTelemetryRequestState()
         //mapView.logo.enabled = false
+        mapView.getMapboxMap().addOnMapLoadedListener(this)
+        mapView.getMapboxMap().addOnCameraChangeListener(this)
 
-        mapView.getMapboxMap().addOnMapIdleListener(this)
-        val locationComponentPlugin = mapView.location2
-        locationComponentPlugin.updateSettings {
-            enabled = true
-        }
-        locationComponentPlugin.updateSettings2 {
-
-        }
 
         val cameraBoundsOptions = CameraBoundsOptions.Builder()
             .maxPitch(10.0)
@@ -111,13 +167,10 @@ class MapActivity : AppCompatActivity(),OnMapIdleListener {
                 latitude = it.latitude
                 mapView.getMapboxMap().setCamera(
                     CameraOptions.Builder()
-                        .zoom(11.0)
+                        .zoom(15.0)
                         .center(Point.fromLngLat(it.longitude, it.latitude))
                         .build()
                 )
-
-
-
 
 
             }
@@ -171,10 +224,12 @@ class MapActivity : AppCompatActivity(),OnMapIdleListener {
         retrieveBtn.setTextColor(Color.BLUE)
     }
 
-
+/*
     fun getTileQuery() {
         Timber.d("startToReivev" + longitude + "lat" + latitude)
+
         val tilequery = MapboxTilequery.builder()
+
             .accessToken(resources.getString(R.string.mapbox_access_token))
             .tilesetIds("mapbox.mapbox-streets-v8")
             .query(Point.fromLngLat(longitude, latitude))
@@ -203,10 +258,41 @@ class MapActivity : AppCompatActivity(),OnMapIdleListener {
             }
         })
     }
-
+*/
     override fun onMapIdle(eventData: MapIdleEventData) {
         Timber.d("onMapIdle: %s", eventData.toString())
     }
 
+    override fun onMapLoaded(eventData: MapLoadedEventData) {
+        Timber.d("onMapLoaded: %s", eventData.toString())
+        val locationComponentPlugin = mapView.location2
+        locationComponentPlugin.updateSettings {
+            enabled = true
+        }
+        locationComponentPlugin.updateSettings2 {
+
+        }
+        mapView.getMapboxMap().addOnMapIdleListener(this)
+    }
+
+    private fun setupGesturesListener() {
+        mapView.gestures.addOnMoveListener(onMoveListener)
+    }
+
+
+
+
+    override fun onCameraChanged(eventData: CameraChangedEventData) {
+        //Timber.d("onCameraChanged: %s", eventData.toString())
+    }
+
+    private fun onCameraTrackingDismissed() {
+        Toast.makeText(this, "onCameraTrackingDismissed", Toast.LENGTH_SHORT).show()
+        mapView.location
+            .removeOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
+        mapView.location
+            .removeOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
+        mapView.gestures.removeOnMoveListener(onMoveListener)
+    }
 
 }
